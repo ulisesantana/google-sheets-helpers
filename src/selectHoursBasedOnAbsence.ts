1cfcee84c12b7b57a6b54efcc60f7e9a3461341e
Ulises Santana <ulisesantana@gmail.com>
2021-06-25 20:04:55 +0100

    ✨️ add select hours based on absence

--- src/selectHoursBasedOnAbsence.ts
+++ src/selectHoursBasedOnAbsence.ts
@@ -1,15 +1,13 @@
 /**
- * Get proportional time for a duration.
+ * Select the hours based on absence if exists.
  *
  * @param {string} absence The absence duration.
  * @param {string} expected The expected duration for the day hours.
- * @param {string} proportional The proportional duration for the day hours based on total.
- * @param {string} total The total duration for the day hours.
- * @return {number} The proportional duration for the given total duration.
+ * @param {number} proportion The proportional duration for the day hours based on total hours for that day.
+ * @return {string} The hours.
  * @customfunction
  */
-function SELECT_HOURS_BASED_ON_ABSENCE (absence, expected, proportional, total) {
-  const getProportion = (proportion, total) => fromDurationToMinutes(proportion) / fromDurationToMinutes(total)
+function SELECT_HOURS_BASED_ON_ABSENCE (absence, expected, proportion) {
   // Only valid for cases with less than 24 hours
   function fromMinutesToDuration (minutes) {
     const timeInMs = minutes * 60 * 1000
@@ -23,18 +21,18 @@ function SELECT_HOURS_BASED_ON_ABSENCE (absence, expected, proportional, total) 
     return hours * 60 + minutes + seconds / 60
   }
   return absence !== '#N/A'
-    ? fromMinutesToDuration(fromDurationToMinutes(expected) - fromDurationToMinutes(absence) * getProportion(proportional, total))
+    ? fromMinutesToDuration(fromDurationToMinutes(expected) - fromDurationToMinutes(absence) * proportion)
     : fromMinutesToDuration(fromDurationToMinutes(expected))
 }
 
-describe('Get proportional duration should', () => {
-  it('compute proportional duration for the given values', () => {
-    expect(SELECT_HOURS_BASED_ON_ABSENCE('4:00:00', '8:00:00', '7:00:00', '8:00:00')).toBe('04:30:00')
-    expect(SELECT_HOURS_BASED_ON_ABSENCE('4:00:00', '3:00:00', '7:00:00', '7:00:00')).toBe('01:00:00')
-    expect(SELECT_HOURS_BASED_ON_ABSENCE('4:00:00', '4:00:00', '1:00:00', '7:00:00')).toBe('03:25:42')
-    expect(SELECT_HOURS_BASED_ON_ABSENCE('2:00:00', '8:00:00', '7:00:00', '8:00:00')).toBe('06:15:00')
-    expect(SELECT_HOURS_BASED_ON_ABSENCE('4:00:00', '00:00:00', '1:00:00', '8:00:00')).toBe('00:30:00')
-    expect(SELECT_HOURS_BASED_ON_ABSENCE('#N/A', '8:00:00', '7:00:00', '8:00:00')).toBe('08:00:00')
-    expect(SELECT_HOURS_BASED_ON_ABSENCE('#N/A', '00:00:00', '1:00:00', '8:00:00')).toBe('00:00:00')
+describe('SELECT_HOURS_BASED_ON_ABSENCE should', () => {
+  it('retrieve the correct hours based on the given values', () => {
+    expect(SELECT_HOURS_BASED_ON_ABSENCE('4:00:00', '8:00:00', 0.875)).toBe('04:30:00')
+    expect(SELECT_HOURS_BASED_ON_ABSENCE('4:00:00', '3:00:00', 0.875)).toBe('00:30:00')
+    expect(SELECT_HOURS_BASED_ON_ABSENCE('4:00:00', '4:00:00', 0.125)).toBe('03:30:00')
+    expect(SELECT_HOURS_BASED_ON_ABSENCE('2:00:00', '8:00:00', 0.875)).toBe('06:15:00')
+    expect(SELECT_HOURS_BASED_ON_ABSENCE('4:00:00', '00:00:00', 0.125)).toBe('00:30:00')
+    expect(SELECT_HOURS_BASED_ON_ABSENCE('#N/A', '8:00:00', 0.875)).toBe('08:00:00')
+    expect(SELECT_HOURS_BASED_ON_ABSENCE('#N/A', '00:00:00', 0.125)).toBe('00:00:00')
   })
 })
